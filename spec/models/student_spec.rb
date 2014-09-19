08691ed787b9c3236cad7a88f19c0462e47ce030
epicodus <student@epicodus.com>
2014-09-19 12:17:24 -0700

    add validation tests for github username

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Student do
   it { should validate_presence_of :name }
   it { should validate_presence_of :season }
-
+  it { should validate_presence_of :year }
+  it { should validate_presence_of :github_username }
 end
