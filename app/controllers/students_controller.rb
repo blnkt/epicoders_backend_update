5ba1faf99fe321c63f9be58b4d006375afc7c778
epicodus <student@epicodus.com>
2014-09-19 14:16:06 -0700

    allow access to user json

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => :create
 
   respond_to :html, :json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :only => :sessions
+  devise_for :users, :only => :sessions do
+    devise_for :users, :only => :users
+  end
   root to: "students#index"
 
   resources :students, :except => [:edit, :update, :new]
